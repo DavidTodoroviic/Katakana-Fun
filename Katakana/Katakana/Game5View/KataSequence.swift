376a610073ab09788dc1b6e6b837459ec7eb87a9
DavidTodoroviic <89335261+DavidTodoroviic@users.noreply.github.com>
2023-05-25 20:24:10 +1000

    more combinations added

--- Katakana/Katakana/Game5View/KataSequence.swift
+++ Katakana/Katakana/Game5View/KataSequence.swift
@@ -15,149 +15,149 @@ struct GlobalFunctions1 {
     static func getKat() {
         
         if randomWord == "icecream" {
-            providedCharacters = ["ア", "イ", "ス", "ク", "リ", "ーム", "テ"]
-            }
-            else if randomWord == "apartment" {
-                providedCharacters = ["ア", "パ", "ー", "ト", "メ", "ン", "ト"]
-            }
-            else if randomWord == "banana" {
-                providedCharacters = ["バ", "ナ", "ナ"]
-            }
-            else if randomWord == "band" {
-                providedCharacters = ["バ", "ン", "ド"]
-            }
-            else if randomWord == "bus" {
-                providedCharacters = ["バ", "ス"]
-            }
-            else if randomWord == "bed" {
-                providedCharacters = ["ベ", "ッ", "ド"]
-            }
-            else if randomWord == "bench" {
-                providedCharacters = ["ベ", "ン", "チ"]
-            }
-            else if randomWord == "bell" {
-                providedCharacters = ["ベ", "ル"]
-            }
-            else if randomWord == "beer" {
-                providedCharacters = ["ビ", "ー", "ル"]
-            }
-            else if randomWord == "bowling" {
-                providedCharacters = ["ボ", "ウ", "リ", "ン", "グ"]
-            }
-            else if randomWord == "team" {
-                providedCharacters = ["チ", "ー", "ム"]
-            }
-            else if randomWord == "cheese" {
-                providedCharacters = ["チ", "ー", "ズ"]
-            }
-            else if randomWord == "dance" {
-                providedCharacters = ["ダ", "ン", "ス"]
-            }
-            else if randomWord == "date" {
-                providedCharacters = ["デ", "ー", "ト"]
-            }
-            else if randomWord == "department store" {
-                providedCharacters = ["デ", "パ", "ー", "ト", "メ", "ン", "ト", " ", "ス", "ト", "ア"]
-            }
-            else if randomWord == "dessert" {
-                providedCharacters = ["デ", "ザ", "ー", "ト"]
-            }
-            else if randomWord == "door" {
-                providedCharacters = ["ド", "ア"]
-            }
-            else if randomWord == "drum" {
-                providedCharacters = ["ド", "ラ", "ム"]
-            }
-            else if randomWord == "elevator" {
-                providedCharacters = ["エ", "レ", "ベ", "ー", "タ", "ー"]
-            }
-            else if randomWord == "France" {
-                providedCharacters = ["フ", "ラ", "ン", "ス"]
-            }
-            else if randomWord == "football" {
-                providedCharacters = ["フ", "ット", "ボ", "ー", "ル"]
-            }
-            else if randomWord == "gum" {
-                providedCharacters = ["ガ", "ム"]
-            }
-            else if randomWord == "game" {
-                providedCharacters = ["ゲ", "ー", "ム"]
-            }
-            else if randomWord == "gate" {
-                providedCharacters = ["ゲ", "ー", "ト"]
-            }
-            else if randomWord == "gift" {
-                providedCharacters = ["ギ", "フ", "ト"]
-            }
-            else if randomWord == "guitar" {
-                providedCharacters = ["ギ", "タ", "ー"]
-            }
-            else if randomWord == "golf" {
-                providedCharacters = ["ゴ", "ル", "フ"]
-            }
-            else if randomWord == "glass" {
-                providedCharacters = ["グ", "ラ", "ス"]
-            }
-            else if randomWord == "group" {
-                providedCharacters = ["グ", "ル", "ー", "プ"]
-            }
-            else if randomWord == "hamburger" {
-                providedCharacters = ["ハ", "ン", "バ", "ー", "ガ", "ー"]
-            }
-            else if randomWord == "heater" {
-                providedCharacters = ["ヒ", "ー", "タ", "ー"]
-            }
-            else if randomWord == "hotel" {
-                providedCharacters = ["ホ", "テ", "ル"]
-            }
-            else if randomWord == "internet" {
-                providedCharacters = ["イン", "タ", "ー", "ネ", "ット"]
-            }
-            else if randomWord == "jeans" {
-                providedCharacters = ["ジ", "ー", "ン", "ズ"]
-            }
-            else if randomWord == "card" {
-                providedCharacters = ["カ", "ー", "ド"]
-            }
-            else if randomWord == "camera" {
-                providedCharacters = ["カ", "メ", "ラ"]
-            }
-            else if randomWord == "Canada" {
-                providedCharacters = ["カ", "ナ", "ダ"]
-            }
-            else if randomWord == "cake" {
-                providedCharacters = ["ケ", "ー", "キ"]
-            }
-            else if randomWord == "case" {
-                providedCharacters = ["ケ", "ー", "ス"]
-            }
-            else if randomWord == "key" {
-                providedCharacters = ["キ", "ー"]
-            }
-            else if randomWord == "coffee" {
-                providedCharacters = ["コ", "ー", "ヒ", "ー"]
-            }
-            else if randomWord == "cola" {
-                providedCharacters = ["コ", "ー", "ラ"]
-            }
-            else if randomWord == "class" {
-                providedCharacters = ["ク", "ラ", "ス"]
-            }
-            else if randomWord == "Christmas" {
-                providedCharacters = ["ク", "リ", "ス", "マ", "ス"]
-            }
-            else if randomWord == "melon" {
-                providedCharacters = ["メ", "ロ", "ン"]
-            }
-            else if randomWord == "meat pie" {
-                providedCharacters = ["ミ", "ー", "ト", "パ", "イ"]
-            }
-            else if randomWord == "milk" {
-                providedCharacters = ["ミ", "ル", "ク"]
-            }
-            else if randomWord == "model" {
-                providedCharacters = ["モ", "デ", "ル"]
-            }
+            providedCharacters = ["ア","イ","ス","ク","リー","ム"]
+        }
+        else if randomWord == "apartment" {
+            providedCharacters = ["ア","パ","ー","ト","リ","ム","ス"]
+        }
+        else if randomWord == "banana" {
+            providedCharacters = ["バ","ナ","ナ","ロ","ベ","ト","ラ"]
+        }
+        else if randomWord == "band" {
+            providedCharacters = ["バ","ン","ド","ネ","ナ","ス","ワ"]
+        }
+        else if randomWord == "bus" {
+            providedCharacters = ["バ","ス","セ","オ","ウ","エ","ワ"]
+        }
+        else if randomWord == "bed" {
+            providedCharacters = ["ベ","ッ","ド","ス","ネ","チ","ロ"]
+        }
+        else if randomWord == "bench" {
+            providedCharacters = ["ベ","ン","チ","ト","リ","ケ","セ"]
+        }
+        else if randomWord == "bell" {
+            providedCharacters = ["ベ","ル","セ","リ","チ","ク","コ"]
+        }
+        else if randomWord == "beer" {
+            providedCharacters = ["ビ","ー","ル","ロ","ソ","ウ","エ"]
+        }
+        else if randomWord == "bowling" {
+            providedCharacters = ["ボ","ー","リ","ン","グ","ウ","エ"]
+        }
+        else if randomWord == "team" {
+            providedCharacters = ["チ","ー","ム","タ","ツ","ナ","セ"]
+        }
+        else if randomWord == "cheese" {
+            providedCharacters = ["チ","ー","ズ","ネ","リ","ケ","シ"]
+        }
+        else if randomWord == "dance" {
+            providedCharacters = ["ダ","ン","ス","ベ","オ","ダ","ニ"]
+        }
+        else if randomWord == "date" {
+            providedCharacters = ["デ","ー","ト","ウ","マ","ラ","ロ"]
+        }
+        else if randomWord == "department store" {
+            providedCharacters = ["デ","パ","ー","ト","ロ","エ","フ"]
+        }
+        else if randomWord == "dessert" {
+            providedCharacters = ["デ","ザ","ー","ト","コ","ク","メ"]
+        }
+        else if randomWord == "door" {
+            providedCharacters = ["ド","ア","オ","テ","ク","リ","イ"]
+        }
+        else if randomWord == "drum" {
+            providedCharacters = ["ド","ラ","ム","イ","ク","シ","テ"]
+        }
+        else if randomWord == "elevator" {
+            providedCharacters = ["エ","レ","ベ","ー","タ","ー","マ"]
+        }
+        else if randomWord == "France" {
+            providedCharacters = ["フ","ラ","ン","ス","フ","メ","シ"]
+        }
+        else if randomWord == "football" {
+            providedCharacters = ["フ","ッ","ト","ボ","ー","ル","ベ"]
+        }
+        else if randomWord == "gum" {
+            providedCharacters = ["ガ","ム","ベ","ハ","マ","キ","ナ"]
+        }
+        else if randomWord == "game" {
+            providedCharacters = ["ゲ","ー","ム","ポ","ロ","ウ","エ"]
+        }
+        else if randomWord == "gate" {
+            providedCharacters = ["ゲ","ー","ト","ベ","ソ","オ","ヒ"]
+        }
+        if randomWord == "gift" {
+            providedCharacters = ["ギ","フ","ト","エ","リ","チ","タ"]
+        }
+        else if randomWord == "guitar" {
+            providedCharacters = ["ギ","タ","ー","ロ","コ","ス","マ"]
+        }
+        else if randomWord == "golf" {
+            providedCharacters = ["ゴ","ル","フ","イ","ナ","タ","ヒ"]
+        }
+        else if randomWord == "glass" {
+            providedCharacters = ["グ","ラ","ス","ト","メ","マ","メ"]
+        }
+        else if randomWord == "group" {
+            providedCharacters = ["グ","ル","ー","プ","ク","コ","ヒ"]
+        }
+        else if randomWord == "hamburger" {
+            providedCharacters = ["ハ","ン","バ","ー","ガ","ー","シ"]
+        }
+        else if randomWord == "heater" {
+            providedCharacters = ["ヒ","ー","タ","ー","キ","ポ","コ"]
+        }
+        else if randomWord == "hotel" {
+            providedCharacters = ["ホ","テ","ル","ヒ","メ","ク","メ"]
+        }
+        else if randomWord == "internet" {
+            providedCharacters = ["イ","ン","タ","ー","ネ","ッ","ト"]
+        }
+        else if randomWord == "jeans" {
+            providedCharacters = ["ジ","ー","ン","ズ","タ","ハ","リ"]
+        }
+        else if randomWord == "card" {
+            providedCharacters = ["カ","ー","ド","ナ","テ","チ","リ"]
+        }
+        else if randomWord == "camera" {
+            providedCharacters = ["カ","メ","ラ","ト","リ","オ","キ"]
+        }
+        else if randomWord == "Canada" {
+            providedCharacters = ["カ","ナ","ダ","メ","ウ","ミ","コ"]
+        }
+        else if randomWord == "cake" {
+            providedCharacters = ["ケ","ー","キ","シ","カ","チ","ソ"]
+        }
+        else if randomWord == "case" {
+            providedCharacters = ["ケ","ー","ス","フ","ハ","エ","シ"]
+        }
+        else if randomWord == "key" {
+            providedCharacters = ["キ","ー","ヒ","リ","ハ","ジ","オ"]
+        }
+        else if randomWord == "coffee" {
+            providedCharacters = ["コ","ー","ヒ","ー","ト","ミ","ジ"]
+        }
+        else if randomWord == "cola" {
+            providedCharacters = ["コ","ー","ラ","リ","ポ","ア","ス"]
+        }
+        else if randomWord == "class" {
+            providedCharacters = ["ク","ラ","ス","ソ","キ","バ","ハ"]
+        }
+        else if randomWord == "Christmas" {
+            providedCharacters = ["ク","リ","ス","マ","ス","バ","ナ"]
+        }
+        else if randomWord == "melon" {
+            providedCharacters = ["メ","ロ","ン","ド","ジ","マ","ケ"]
+        }
+        else if randomWord == "meat pie" {
+            providedCharacters = ["ミ","ー","ト","パ","イ","ン","ハ"]
+        }
+        else if randomWord == "milk" {
+            providedCharacters = ["ミ","ル","ク","メ","ネ","ソ","タ"]
+        }
+        else if randomWord == "model" {
+            providedCharacters = ["モ","デ","ル","イ","チ","バ","メ"]
+        }
             else if randomWord == "knife" {
                 providedCharacters = ["ナ", "イ", "フ"]
             }
