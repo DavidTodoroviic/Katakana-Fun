cd7e98b4b72a61d2f9a2c55f6f613d6709de3310
WallyCars <101530341+WallyCars@users.noreply.github.com>
2023-06-06 18:05:56 +1000

    Look's good team

--- Katakana/Katakana/InfoView.swift
+++ Katakana/Katakana/InfoView.swift
@@ -26,7 +26,7 @@ struct InfoView: View {
                     Text("Welcome to the Japres Japanese Vocabulary Study Buddy App! This app has been created to bring a little enjoyment to learning Japanese. The study section contains vocabulary cards with anime characters and includes a Japanese word written in hiragana/katakana, kanji and romaji as well as the English meaning. You can choose from a range of topics and once you have learned each topic you can play the games to practise your Japanese knowledge. The games have been designed to allow you to play as soon as you have learned one topic. You can select the topic(s) you have learned at the start of each game and only the vocabulary related to those topics will appear in the games. Depending on your Japanese knowledge, you can choose to play each game in ‘romaji’ (English letters for each word) or in ‘kana’ (hiragana and katakana scripts). We hope by playing the games you will learn Japanese vocabulary easily and have fun while learning. \n\nA special thankyou to my friend, Atsuya for recording the authentic Japanese pronunciation audio files used throughout the app.\n\nFor more information, fun ideas and free resources, please visit www.japres.com.au")
                     .font(.custom("Comic Sans MS", size: textSize))
                     .frame(maxWidth: 350, alignment: .leading)
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
                 } // close brace for scroll view
                 
                 Button(action: {
